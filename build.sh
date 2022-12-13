6b3ad5adc6a95f14916df91f1560d73c6621fa3d
skperez <stepheny.k.perez@jpl.nasa.gov>
2022-12-13 01:25:26 -0800

    Disable hashes when exporting poetry env

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -Rf venv build;mkdir venv
 
-poetry export -f requirements.txt --output requirements.txt
+poetry export -f requirements.txt --output requirements.txt --without-hashes
 pip3 install -r requirements.txt  --target ./venv
 cp ./*.py ./venv
 cd venv; zip -r ../artifact.zip .
